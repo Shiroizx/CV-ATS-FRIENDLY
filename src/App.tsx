24f09a61341c6ba0710f57330f0ffb32ca7f2315
Shiroizx <kzxxx01@gmail.com>
2025-12-23 15:35:08 +0700

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import MainPage from './pages/MainPage';
 import CVBuilderPage from './pages/CVBuilderPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/builder" element={<CVBuilderPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/builder" element={<CVBuilderPage />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
